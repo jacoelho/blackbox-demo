240e98d2ed92e3d5705aa6e1d45790cd606c3bb1
José Coelho <jose.alberto.coelho@gmail.com>
2015-02-11 14:13:11 +0000

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+config=$(mktemp -t gpg-XXXXX)
+
+# virtualbox entropy not enough
+curl -s http://releases.ubuntu.com/14.04/ubuntu-14.04-desktop-amd64.iso > /dev/urandom &
+
+echo "Creating GPG key..."
+cat >${config}<<EOF
+%echo Generating a basic OpenPGP key
+Key-Type: DSA
+Key-Length: 1024
+Subkey-Type: ELG-E
+Subkey-Length: 1024
+Name-Real: blackbox dem
+Name-Comment: blackbox demo
+Name-Email: blackbox-demo@demo
+Expire-Date: 0
+Passphrase: demo
+%commit
+EOF
+
+gpg --batch --gen-key ${config}
+
+echo yes | blackbox_initialize
+
+git commit -m'INITIALIZE BLACKBOX' keyrings .gitignore
